3c971f1b7f4b8f3542220e1196f7467e3f633bfe
benbalter <ben@balter.com>
2011-05-28 12:59:05 -0400

    initial commit

---
+++ gwhaid.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: GW Hatchet Alumni Information Database
+Plugin URI: http://alumni.gwhatchet.com
+Description: Stores info about people
+Version: 0.1a
+Author: Benjamin J. Balter, Alex Byers, Andrew Nacin
+Author URI: http://alumni.gwhatchet.com
+License: GPL2
+*/
+
+class gwhaid {
+
+
+	/**
+	 * Constructor function for hooks
+	 */
+	function gwhaid() {
+	
+	
+	}
+
+}
+
+$gwhaiud = new gwhaid();
+
+?>
